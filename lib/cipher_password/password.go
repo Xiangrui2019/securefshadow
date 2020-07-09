3ca8331bb8a2a07ad142c14f88dade1ece62ea2c
GoGoGo2019 <xiangrui@aiursoft.com>
2020-07-09 10:17:18 +0000

    add a basic cipher

---
+++ lib/cipher_password/password.go
@@ -0,0 +1,49 @@
+package lightsocks
+
+import (
+	"encoding/base64"
+	"errors"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const passwordLength = 256
+
+type password [passwordLength]byte
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+// 采用base64编码把密码转换为字符串
+func (password *password) String() string {
+	return base64.StdEncoding.EncodeToString(password[:])
+}
+
+// 解析采用base64编码的字符串获取密码
+func ParsePassword(passwordString string) (*password, error) {
+	bs, err := base64.StdEncoding.DecodeString(strings.TrimSpace(passwordString))
+	if err != nil || len(bs) != passwordLength {
+		return nil, errors.New("不合法的密码")
+	}
+	password := password{}
+	copy(password[:], bs)
+	bs = nil
+	return &password, nil
+}
+
+// 产生 256个byte随机组合的 密码，最后会使用base64编码为字符串存储在配置文件中
+// 不能出现任何一个重复的byte位，必须又 0-255 组成，并且都需要包含
+func RandPassword() string {
+	// 随机生成一个由  0~255 组成的 byte 数组
+	intArr := rand.Perm(passwordLength)
+	password := &password{}
+	for i, v := range intArr {
+		password[i] = byte(v)
+		if i == v {
+			return RandPassword()
+		}
+	}
+	return password.String()
+}
